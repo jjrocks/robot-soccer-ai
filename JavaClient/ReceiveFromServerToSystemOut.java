93a98ef0148a88d38ab4490d8351fded98b800d8
Lucy McKnight <mcknighl@lafayette.edu>
2015-10-02 03:54:13 -0400

    actually the problem mentioned before is fixed if oyu just init to version 7. not sure how to fix for more recent versions yet. this is a fast hacky version i made at 3am to see if i could make the player run at the ball. if you manually init and then type in (move -10 0) to position the player and then start the kickoff from the monitor then the player does try to run at the ball but then teleports away. no idea why this is but we had this problem when manually controlling via rcssclient as well so basically wtf

--- JavaClient/ReceiveFromServerToSystemOut.java
+++ JavaClient/ReceiveFromServerToSystemOut.java
@@ -17,7 +17,7 @@ public class ReceiveFromServerToSystemOut implements Runnable
 	{
 		while(true)
 		{
-			System.out.println("Receiving: " + receive());
+			parse(receive());
 		}
 	}
 
@@ -35,5 +35,74 @@ public class ReceiveFromServerToSystemOut implements Runnable
 		}
 
 		return new String(buffer);
+	}
+
+	private void parse(String input)
+	{
+		if(input.substring(0,4).equals("(see"))
+		{
+			int index = input.indexOf("(b)");
+			if(index > -1)
+			{
+				String blah = input.substring(index+3);
+				Scanner sc = new Scanner(blah);
+				System.out.println(blah);
+				double ballDist = sc.nextDouble();
+				String a = sc.next();
+				System.out.println("a: " + a);
+				if(a.substring(a.length()-1).equals(")"))
+				{
+					a = a.substring(0,a.length()-1);
+					System.out.println("modded a: " + a);
+				}
+				double ballDir = Double.parseDouble(a);
+				if(ballDir > 50)
+				{
+					send("(turn " + ballDir + ")");
+				}
+				else
+				{
+					if(ballDist > 1)
+					{
+						send("(dash 10)");
+					}
+					else
+					{
+						send("(kick 10 0)");
+					}
+				}
+			}
+			else
+			{
+				send("(turn 50)");
+			}
+		}
+	}
+
+	private void send(String message)
+	{
+		System.out.println("Sending: " + message);
+
+		byte[] buffer = message.getBytes();
+
+		try
+		{
+			DatagramPacket pkt = new DatagramPacket(buffer, buffer.length, 
+						InetAddress.getByName("compute212.cs.lafayette.edu"), 6000);
+			
+			try
+			{
+				socket.send(pkt);
+			}
+			catch (IOException e)
+			{
+				System.out.println("Socket sending error: " + e);
+			}		
+
+		}
+		catch (UnknownHostException e)
+		{
+			System.out.println("Unknown host exception: " + e);
+		}
 	}
 }
